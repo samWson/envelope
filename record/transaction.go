35ac6c490f3c1c1c2f0cacd4907bb9aefc74f67f
Samuel Wilson <samWson@users.noreply.github.com>
2017-11-03 07:55:32 +1300

    Document package record

--- record/transaction.go
+++ record/transaction.go
@@ -1,5 +1,30 @@
+// MIT License
+//
+// Copyright (c) 2017 Samuel Wilson <samWson@users.noreply.github.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Package record implements types representing transactions of money in a budget.
 package record
 
+// Transaction is a single record of monetary change in a budget e.g. income
+// entered, expense remove, or money added to an envelope.
 type Transaction struct {
 	Category string
 	Amount   float64
